39eaa78cc6c9fec502dc50af2687eb40b64774f7
CoolTrainerEX <92789937+CoolTrainerEX@users.noreply.github.com>
2025-03-16 15:35:43 +0800

    yes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  Injectable,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  RouterStateSnapshot,
+  TitleStrategy,
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { Title } from '@angular/platform-browser';
+
+@Injectable({ providedIn: 'root' })
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`${title} | Official Weirdo website`);
+    }
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
   ],
 };
---
+++ src/app/directory/directory.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directory',
+  imports: [],
+  templateUrl: './directory.component.html',
+  styleUrl: './directory.component.css',
+})
+export class DirectoryComponent {}
